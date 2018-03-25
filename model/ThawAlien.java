b96dced89f784aeaa0e760fa0fbeb7151059bbd1
murokami <37623405+murokami@users.noreply.github.com>
2018-03-25 10:10:49 +0300

    Add files via upload

---
+++ model/ThawAlien.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
+public class ThawAlien {
+    public static void main(String[] args) throws Exception {
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream(
+                new File("X.files")));
+        Object mystery = in.readObject();
+        System.out.println(mystery.getClass());
+    }
+}
